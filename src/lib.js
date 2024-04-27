57ecb4061ae8e594fa03b1a834d8f992b22f0071
Etcetera Zahz <157307429+GenoyVI@users.noreply.github.com>
2024-04-27 11:30:38 -0500

    Alphabatized The Chapion Array In Data.js and Created A File Hosting A Re-Usable Function Called lib.js

---
+++ src/lib.js
@@ -0,0 +1,55 @@
+const CHAMPIONS = [
+    "Axiom",
+    "Ilja Dragunov",
+    "Charlie Dempsey",
+    "Nathan Frazer",
+    "Roxanne Perez",
+    "Damien Priest",
+    "Cody Rhodes",
+    "Awesome Truth",
+    "A-Town Down Under",
+    "The Kabuki Warriors",
+
+
+
+
+   
+
+
+  const BELTS = [
+    "Undisputed WWE Champion",
+    "World Heavyweight Champion",
+    "Women's World Champion",
+    "WWE Women’s Champion",
+    "Intercontinental Champion",
+    "United States Champion",
+    "World Tag Team Champions",
+    "SmackDown Tag Team Champions",
+    "WWE Women's Tag Team Champions",
+    "NXT Champion",
+    "NXT Women's Champion",
+    "NXT North American Champion",
+    "NXT Tag Team Champions",
+    "NXT Heritage Cup Champion",
+  ];
+function sortArrayBy(array, position = "last") {
+    if (position === "first") {
+        return array.sort((a, b) => {
+            const firstNameA = a.split(" ")[0];
+            const firstNameB = b.split(" ")[0];
+            return firstNameA.localeCompare(firstNameB);
+        });
+    } else {
+        return array.sort((a, b) => {
+            const lastNameA = a.split(" ").pop();
+            const lastNameB = b.split(" ").pop();
+            return lastNameA.localeCompare(lastNameB);
+        });
+    }
+}
+
+const sortedChampionsByLastName = sortArrayBy(CHAMPIONS);
+console.log(sortedChampionsByLastName);
+
+const sortedChampionsByFirstName = sortArrayBy(CHAMPIONS, "first");
+console.log(sortedChampionsByFirstName);
